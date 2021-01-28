9388d3acc0ea783ef2eb47b2b595c8ea530c90c3
Ivan Trendafilov <ivan_trendafilov@starsgroup.com>
2021-01-28 11:26:28 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # grpc-load-balancing
 Grpc with load balancing strategy on NGINX config
+
+## RUN
+cd main
+docker-compose up -d
+
+curl http://localhost:81/top
---
+++ client/src/main/java/ru/hixon/movieclient/GrpcConfiguration.java
@@ -0,0 +1,24 @@
+package ru.hixon.movieclient;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.hixon.movies.MoviesRatingGrpc;
+
+@Configuration
+public class GrpcConfiguration {
+
+    @Bean
+    public ManagedChannel managedChannel() {
+        return ManagedChannelBuilder.forAddress("nginx", 80)
+                .usePlaintext(true)
+                .build();
+    }
+
+    @Bean
+    public MoviesRatingGrpc.MoviesRatingFutureStub moviesRatingStub(
+            ManagedChannel managedChannel) {
+        return MoviesRatingGrpc.newFutureStub(managedChannel);
+    }
+}
